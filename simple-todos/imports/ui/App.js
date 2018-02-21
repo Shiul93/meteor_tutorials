17d2154c5e54d8e7077861d4e139cba9de223c10
Shiul93 <lfllamas93@gmail.com>
2018-02-21 11:15:06 +0100

    Finish step 10

--- simple-todos/imports/ui/App.js
+++ simple-todos/imports/ui/App.js
@@ -25,12 +25,8 @@ class App extends Component {
     // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
  
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username,  // username of logged in user
-    });
+    Meteor.call('tasks.insert', text);
+
  
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
@@ -47,9 +43,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+ 
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
@@ -91,7 +96,10 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+  Meteor.subscribe('tasks');
+
   return {
+    
         //Accesible mediante this.props
         incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
